56dedbe39358a50e393392c57c5ecefa54873af3
z <christopher.zell@gmail.com>
2020-08-18 21:24:52 -0400

    first commit

---
+++ cc/Release/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+LCD.cpp
+
+Interpreter.cpp
+
+main.cpp
+
+Serial.cpp
+
+Value.cpp
+
---
+++ cc/Value.h
@@ -0,0 +1,73 @@
+/*
+* Value.h
+*
+* Created: 12/14/2018 8:36:21 PM
+* Author: z
+*/
+
+#include "main.h"
+
+#ifndef __VALUE_H__
+#define __VALUE_H__
+#define TRUE_INT_VAL -1
+#define FALSE_INT_VAL 0
+
+enum TYPE { INTEGER, STRING, BOOLEAN, FLOAT };
+
+class Value
+{
+
+	public:
+	int* intArray;
+	double* dblArray;
+	int number;
+	double floatNumber;
+	char str[MAXSTRLENGTH];
+	bool isArray;
+	int arraySize;
+	TYPE type;
+	bool bval;
+	bool ToBoolean();
+	char* ToString();
+	int ToInt();
+	double ToDouble();
+	Value();
+	Value(int i);
+	Value(char i);
+	Value(const char* s);
+	Value(bool b);
+	Value(TYPE t, int size);
+	Value(double i);
+	Value index_array(int index);
+	void update_array(int index, int val);
+	void update_array(int index, double val);
+	friend Value operator+(const Value& v1);
+	friend Value operator-(const Value& v1);
+	friend Value operator+(const Value& v1, const Value &v2);
+	friend Value operator-(const Value& v1, const Value &v2);
+	friend Value operator*(const Value& v1, const Value &v2);
+	friend Value operator/(const Value& v1, const Value &v2);
+	friend Value operator%(const Value& v1, const Value &v2);
+	friend Value operator<<(const Value& v1, const Value &v2);
+	friend Value operator>>(const Value& v1, const Value &v2);
+	friend Value operator&(const Value& v1, const Value &v2);
+	friend Value operator|(const Value& v1, const Value &v2);
+	friend Value operator!(const Value& v1);
+	friend Value operator^(const Value& v1, const Value &v2);
+	friend Value operator==(const Value& v1, const Value &v2);
+	friend Value operator!=(const Value& v1, const Value &v2);
+	friend Value operator>(const Value& v1, const Value &v2);
+	friend Value operator>=(const Value& v1, const Value &v2);
+	friend Value operator<(const Value& v1, const Value &v2);
+	friend Value operator<=(const Value& v1, const Value &v2);
+
+
+}; //Value
+
+struct Token {
+	TokenType type;
+	Value value;
+};
+
+
+#endif //__VALUE_H__
