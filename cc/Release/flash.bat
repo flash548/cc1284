7d67c9e28774a5aede8edc206157246344252d1f
Christopher Zell <christopher.zell@gmail.com>
2022-07-30 19:19:59 -0400

    clean up

--- cc/Release/flash.bat
+++
@@ -1,5 +0,0 @@
-avrdude -c usbtiny -p m1284p -V -U flash:w:cc1284.hex
-
-REM -U lfuse:w:0xe0:m -U hfuse:w:0xd9:m -U efuse:w:0xff:m
-REM for EESAVE use:
-REM avrdude -c usbtiny -p m1284p -U lfuse:w:0xe0:m -U hfuse:w:0xd1:m -U efuse:w:0xff:m
